6dc861471bd4c8c90191e9e815d25113ac8224d3
John Tauber <john.tauber@gmail.com>
2017-10-26 23:52:03 -0400

    edit formatting for readability

--- image_directory_sort.py
+++ image_directory_sort.py
@@ -2,21 +2,22 @@ import pandas as pd
 import os.path
 import shutil
 
-meta = pd.read_pickle('meta.pkl')
-
-dir_dict = {}
 
 # generate dictionary for filename: desired subdirectory
+meta = pd.read_pickle('meta.pkl')
+dir_dict = {}
+
 for i in range(len(meta)):
     row = meta.loc[i,:]
     dir_dict[row['id']+'.png'] = [row['category'], row['obj'], row['var']]
 
+
+# make copies of each image in new folders
 folder_path = "images"
 new_folder = "sorted_images"
 
 images = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
-# make copies of each image in new folders
 for image in images:
     folder_list = dir_dict[image]
     folder_name = folder_list[0]+'/'+folder_list[1]+'/'+folder_list[2]
