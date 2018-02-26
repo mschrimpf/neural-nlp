81fc6ee2c69bf7f4b59d3e9f1299a2b58317fa79
Martin Schrimpf <m4rtinsch@gmail.com>
2018-02-26 01:23:17 -0500

    split main method into own file
    otherwise, there are problems with pickling Type

--- neural_metrics/metrics/__init__.py
+++ neural_metrics/metrics/__init__.py
@@ -1,7 +1,5 @@
-import argparse
 import logging
 import os
-import sys
 from collections import OrderedDict
 from enum import Enum, auto
 
@@ -84,30 +82,3 @@ def score_model_activations(activations_filepath, regions, model_name=None, map_
     anatomy_score = scores(name='edge_ratio', type=Type.ANATOMY)
     _logger.info("Anatomy score: {}".format(anatomy_score))
     return physiology_scores + [anatomy_score]
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--activations_filepath', type=str, nargs='+',
-                        default=[os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'images', 'sorted',
-                                                              'vgg16-weights_imagenet-activations.pkl'))],
-                        help='one or more filepaths to the model activations')
-    parser.add_argument('--model', type=str, default=None, choices=models.model_mappings.keys(),
-                        help='name of the model. Inferred from `--activations_filepath` if None')
-    parser.add_argument('--regions', type=str, nargs='+', default=['V4', 'IT'], help='region(s) in brain to compare to')
-    parser.add_argument('--map_all_layers', action='store_true', default=True)
-    parser.add_argument('--no-map_all_layers', action='store_false', dest='map_all_layers')
-    parser.add_argument('--log_level', type=str, default='INFO')
-    args = parser.parse_args()
-    logging.basicConfig(stream=sys.stdout, level=logging.getLevelName(args.log_level))
-    _logger.info("Running with args {}".format(vars(args)))
-
-    for activations_filepath in args.activations_filepath:
-        print(activations_filepath)
-        scores = score_model_activations(activations_filepath, regions=args.regions, model_name=args.model,
-                                         map_all_layers=args.map_all_layers)
-        print(scores)
-
-
-if __name__ == '__main__':
-    main()
---
+++ neural_metrics/metrics/__main__.py
@@ -0,0 +1,35 @@
+import argparse
+import logging
+import os
+import sys
+
+from neural_metrics import models, score_model_activations
+
+_logger = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--activations_filepath', type=str, nargs='+',
+                        default=[os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'images', 'sorted',
+                                                              'vgg16-weights_imagenet-activations.pkl'))],
+                        help='one or more filepaths to the model activations')
+    parser.add_argument('--model', type=str, default=None, choices=models.model_mappings.keys(),
+                        help='name of the model. Inferred from `--activations_filepath` if None')
+    parser.add_argument('--regions', type=str, nargs='+', default=['V4', 'IT'], help='region(s) in brain to compare to')
+    parser.add_argument('--map_all_layers', action='store_true', default=True)
+    parser.add_argument('--no-map_all_layers', action='store_false', dest='map_all_layers')
+    parser.add_argument('--log_level', type=str, default='INFO')
+    args = parser.parse_args()
+    logging.basicConfig(stream=sys.stdout, level=logging.getLevelName(args.log_level))
+    _logger.info("Running with args {}".format(vars(args)))
+
+    for activations_filepath in args.activations_filepath:
+        print(activations_filepath)
+        scores = score_model_activations(activations_filepath, regions=args.regions, model_name=args.model,
+                                         map_all_layers=args.map_all_layers)
+        print(scores)
+
+
+if __name__ == '__main__':
+    main()
