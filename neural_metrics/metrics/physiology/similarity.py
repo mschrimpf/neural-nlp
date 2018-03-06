2dfe2a5f7fa179ad92f7ef82f00c844cf53192eb
Martin Schrimpf <m4rtinsch@gmail.com>
2018-03-06 14:33:29 -0500

    make sure corrcoef is in the right shape

--- neural_metrics/metrics/physiology/similarity.py
+++ neural_metrics/metrics/physiology/similarity.py
@@ -10,5 +10,7 @@ def rdm(data):
 def rsa(data):
     _rdm = np.empty((data.shape[0], data.shape[0]))
     for i, j in itertools.combinations_with_replacement(list(range(data.shape[0])), 2):
-        _rdm[i, j] = _rdm[j, i] = np.corrcoef(data[i], data[j])[0, 1]
+        correlation = np.corrcoef(data[i], data[j])
+        assert len(correlation.shape) == 2 and correlation.shape[0] == correlation.shape[1] == 2
+        _rdm[i, j] = _rdm[j, i] = correlation[0, 1]
     return _rdm
