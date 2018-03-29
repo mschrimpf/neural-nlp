f7723acb10c1a2ef49aa69759e6c6b810d553610
Martin Schrimpf <m4rtinsch@gmail.com>
2018-03-29 12:25:23 -0400

    allow a generic combination of feature vectors in word2vec

--- neural_nlp/models/implementations.py
+++ neural_nlp/models/implementations.py
@@ -1,6 +1,6 @@
 import os
 
-import gensim.models.keyedvectors as word2vec
+import gensim.models.keyedvectors as word2vec_storage
 import numpy as np
 
 _ressources_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'ressources', 'models')
@@ -33,30 +33,38 @@ def lm_1b(weights=os.path.join(_ressources_dir, 'lm_1b')):
     return encode
 
 
-def word2vec_mean(weights=os.path.join(_ressources_dir, 'word2vec', 'GoogleNews-vectors-negative300.bin')):
+def _mean_vector(feature_vectors, num_words):
+    assert len(feature_vectors) == num_words
+    assert num_words > 0
+    if num_words == 1:
+        return feature_vectors[0]
+    feature_vectors = np.sum(feature_vectors, axis=0)
+    return np.divide(feature_vectors, num_words)
+
+
+def word2vec(weights=os.path.join(_ressources_dir, 'word2vec', 'GoogleNews-vectors-negative300.bin'),
+             combine_vectors=_mean_vector):
     """
     https://arxiv.org/pdf/1310.4546.pdf
     """
-    model = word2vec.KeyedVectors.load_word2vec_format(weights, binary=True)
+    model = word2vec_storage.KeyedVectors.load_word2vec_format(weights, binary=True)
     index2word_set = set(model.index2word)
 
-    def mean_feature_vector(sentence):
+    def combined_feature_vector(sentence):
         words = sentence.split()
-        feature_vec = None
-        n_words = 0
+        feature_vectors = []
+        num_words = 0
         for word in words:
             if word in index2word_set:
-                n_words += 1
-                feature_vec = np.add(feature_vec, model[word]) if feature_vec is not None else model[word]
-        if n_words > 0:
-            feature_vec = np.divide(feature_vec, n_words)
-        return feature_vec
+                num_words += 1
+                feature_vectors.append(model[word])
+        return combine_vectors(feature_vectors, num_words)
 
-    return mean_feature_vector
+    return combined_feature_vector
 
 
 model_mappings = {
     'skip-thoughts': skip_thoughts,
     'lm_1b': lm_1b,
-    'word2vec': word2vec_mean
+    'word2vec': word2vec
 }
--- tests/models/test_implementations.py
+++ tests/models/test_implementations.py
@@ -1,9 +1,9 @@
-from neural_nlp.models.implementations import word2vec_mean
+from neural_nlp.models.implementations import word2vec
 import numpy as np
 
 
 def test_word2vec():
-    model = word2vec_mean()
+    model = word2vec()
     encoding = model('The quick brown fox jumps over the lazy dog')
     assert isinstance(encoding, np.ndarray)
     assert len(encoding.shape) == 1
