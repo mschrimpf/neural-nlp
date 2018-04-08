648283bd209c90e23ebd7125bfde2ec40af121cc
Martin Schrimpf <m4rtinsch@gmail.com>
2018-04-08 18:36:45 -0400

    load naturalistic stories with pandas and optionally keep metas

--- neural_nlp/data/__init__.py
+++ neural_nlp/data/__init__.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 _data_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'ressources', 'data')
 
@@ -16,10 +17,10 @@ def _diverse_sentences(filepath):
         return f.read().splitlines()
 
 
-def naturalistic_stories(filepath=os.path.join(_data_dir, 'naturalistic_stories', 'all_stories.tok')):
-    with open(filepath) as f:
-        next(f)  # ignore header
-        words = (line.split()[0] for line in f)  # only first word
+def naturalistic_stories(filepath=os.path.join(_data_dir, 'naturalistic_stories', 'all_stories.tok'), keep_meta=False):
+    data = pd.read_csv(filepath, delimiter='\t')
+
+    def apply(words):
         sentences = []
         sentence = ''
         for word in words:
@@ -29,7 +30,13 @@ def naturalistic_stories(filepath=os.path.join(_data_dir, 'naturalistic_stories'
                 sentence = ''
             else:
                 sentence += ' '
-        return sentences
+        return pd.DataFrame({'sentence': sentences, 'sentence_num': list(range(len(sentences)))})
+
+    data = data.groupby('item')['word'].apply(apply).reset_index(level=0)
+    data = data[['item', 'sentence_num', 'sentence']]
+    if keep_meta:
+        return data
+    return data['sentence'].values
 
 
 data_mappings = {
