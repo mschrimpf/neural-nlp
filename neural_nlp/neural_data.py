186a26b106cdbef1ab57ab816d4b2000b4d02a0e
Martin Schrimpf <m4rtinsch@gmail.com>
2018-04-08 18:36:06 -0400

    implement loading neural data

--- neural_nlp/neural_data.py
+++ neural_nlp/neural_data.py
@@ -1,2 +1,39 @@
-def load():
-    raise NotImplementedError()
+import glob
+import logging
+import os
+
+import numpy as np
+import pandas as pd
+import re
+import xarray as xr
+
+neural_data_dir = os.path.join(os.path.dirname(__file__), '..', 'ressources', 'neural_data')
+_logger = logging.getLogger(__name__)
+
+
+def load_rdms(story='Boar', roi_filter='from90to100'):
+    data = []
+    for filepath in glob.glob(os.path.join(
+            neural_data_dir, '{}{}*.csv'.format(story + '_', roi_filter))):
+        _logger.debug("Loading file {}".format(filepath))
+        attributes = re.match('^.*/(?P<story>.*)_from(?P<roi_low>[0-9]+)to(?P<roi_high>[0-9]+)'
+                              '(_(?P<subjects>[0-9]+)Subjects)?\.mat_r(?P<region>[0-9]+).csv', filepath)
+        _data = pd.read_csv(filepath, header=None)
+        num_stimuli = len(_data.columns)
+        assert len(_data) % num_stimuli == 0
+        num_subjects = len(_data) // num_stimuli
+        _data = np.stack([_data.iloc[(subject * num_stimuli):((subject + 1) * num_stimuli)]
+                          for subject in range(num_subjects)])
+        _data = xr.DataArray([_data], coords={
+            'timepoint_left': list(range(num_stimuli)), 'timepoint_right': list(range(num_stimuli)),
+            'region': [int(attributes['region'])],
+            'subject': list(range(num_subjects))},
+                             dims=['region', 'subject', 'timepoint_left', 'timepoint_right'])
+        stimuli_meta = lambda x: (['timepoint_left', 'timepoint_right'],
+                                  np.broadcast_to(1, [num_stimuli, num_stimuli]))
+        _data['story'] = stimuli_meta(attributes['story'])
+        _data['roi_low'] = stimuli_meta(int(attributes['roi_low']))
+        _data['roi_high'] = stimuli_meta(int(attributes['roi_high']))
+        data.append(_data)
+    data = xr.concat(data, 'region')
+    return data
---
+++ tests/test_neural_data.py
@@ -0,0 +1,24 @@
+from neural_nlp import neural_data
+
+
+class TestLoadRdms():
+    def test_boar(self):
+        self._test_story('Boar', num_timepoints=169, num_subjects=39)
+
+    def test_elvis(self):
+        self._test_story('Elvis', num_timepoints=151, num_subjects=36)
+
+    def test_highschool(self):
+        self._test_story('HighSchool', num_timepoints=174, num_subjects=15)
+
+    def test_king_of_birds(self):
+        self._test_story('KingOfBirds', num_timepoints=198, num_subjects=35)
+
+    def test_matchstick_seller(self):
+        self._test_story('MatchstickSeller', num_timepoints=197, num_subjects=15)
+
+    def _test_story(self, story, num_timepoints, num_subjects, num_regions=44):
+        data = neural_data.load_rdms(story)
+        assert len(data['subject']) == num_subjects
+        assert len(data['timepoint_left']) == len(data['timepoint_left']) == num_timepoints
+        assert len(data['region']) == num_regions
