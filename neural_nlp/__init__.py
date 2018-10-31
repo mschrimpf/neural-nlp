f395d0db5601622bde19f4fd3f6048e36b8c8c99
Martin Schrimpf <m4rtinsch@gmail.com>
2018-10-31 15:21:34 -0400

    cross-validate comparisons

--- neural_nlp/__init__.py
+++ neural_nlp/__init__.py
@@ -1,13 +1,15 @@
 import logging
-
-from brainscore.assemblies import DataAssembly
+from brainscore.assemblies import DataAssembly, walk_coords
 from brainscore.metrics.rdm import RDM, RDMSimilarity
-from brainscore.metrics.transformations import CartesianProduct
+from brainscore.metrics.transformations import CartesianProduct, CrossValidation
 from result_caching import store_xarray
 
 from neural_nlp import models
 from neural_nlp.models import get_activations, model_layers
 from neural_nlp.neural_data import load_rdm_sentences as load_neural_rdms
+
+STORIES = ('Boar', 'KingOfBirds', 'Elvis', 'HighSchool')
+STORIES = [f'naturalistic-neural-reduced.{story}' for story in STORIES]
 
 _logger = logging.getLogger(__name__)
 
@@ -56,6 +58,47 @@ def _run(model, layers, stimulus_set):
     cross_layer = CartesianProduct(dividers=['layer'])
     scores = cross_layer(model_activations, apply=benchmark)
     return scores
+
+
+class MultiRegionBenchmark:
+    def __init__(self, target_assembly):
+        self._target_assembly = target_assembly
+        self._metric = RDMSimilarityCrossValidated()
+        self._cross_region = CartesianProduct(dividers=['region'])
+
+    def __call__(self, source_assembly):
+        score = self._cross_region(self._target_assembly,
+                                   apply=lambda region_assembly: self._metric(source_assembly, region_assembly))
+        return score
+
+
+class RDMSimilarityCrossValidated:
+    # adapted from
+    # https://github.com/dicarlolab/brain-score/blob/3d59d7a841fca63a5d346e599143f547560b5082/brainscore/metrics/rdm.py#L8
+
+    class LeaveOneOutWrapper:
+        def __init__(self, metric):
+            self._metric = metric
+
+        def __call__(self, train_source, train_target, test_source, test_target):
+            # compare assemblies for a single split. we ignore the 10% train ("leave-one-out") and only use test.
+            score = self._metric(test_source, test_target)
+            return DataAssembly(score)
+
+    def __init__(self, stimulus_coord='stimulus_sentence'):
+        self._rdm = RDM()
+        self._similarity = RDMSimilarity(comparison_coord=stimulus_coord)
+        self._cross_validation = CrossValidation(test_size=.9,  # leave 10% out
+                                                 split_coord=stimulus_coord, stratification_coord=None)
+
+    def __call__(self, model_activations, target_rdm):
+        model_activations = align(model_activations, target_rdm, on='stimulus_sentence')
+        model_rdm = self._rdm(model_activations)
+        leave_one_out = self.LeaveOneOutWrapper(self._similarity)
+        # multi-dimensional coords with repeated dimensions not yet supported in CrossValidation
+        drop_coords = [coord for coord, dims, value in walk_coords(target_rdm) if dims == ('stimulus', 'stimulus')]
+        target_rdm = target_rdm.drop(drop_coords)
+        return self._cross_validation(model_rdm, target_rdm, apply=leave_one_out)
 
 
 def align(source, target, on):
