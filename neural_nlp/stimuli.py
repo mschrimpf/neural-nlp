385e234e6aee96cf4f6f157c6ab1bd45230e298d
Martin Schrimpf <m4rtinsch@gmail.com>
2019-06-19 17:50:22 -0400

    use pathlib for paths; add story column; get rid of object superclass

--- neural_nlp/stimuli.py
+++ neural_nlp/stimuli.py
@@ -1,14 +1,24 @@
 import os
 
 import pandas as pd
+from pathlib import Path
 
-_data_dir = os.path.join(os.path.dirname(__file__), '..', 'ressources', 'stimuli')
+
+class StimulusSet(pd.DataFrame):
+    _metadata = ["name"]
+
+    @property
+    def _constructor(self):
+        return StimulusSet
 
 
-class DiverseSentences(object):
+_data_dir = Path(os.path.dirname(__file__)) / '..' / 'ressources' / 'stimuli'
+
+
+class DiverseSentences:
     set_mapping = {'1': 'stimuli_384sentences.txt', '2': 'stimuli_243sentences.txt'}
 
-    def __init__(self, stimulus_set_filename, stimuli_dir=os.path.join(_data_dir, 'diverse_sentences')):
+    def __init__(self, stimulus_set_filename, stimuli_dir=_data_dir / 'diverse_sentences'):
         self._filepath = os.path.join(stimuli_dir, stimulus_set_filename)
 
     def __call__(self):
@@ -16,17 +26,16 @@ class DiverseSentences(object):
             return f.read().splitlines()
 
 
-class NaturalisticStories(object):
-    set_mapping = {'Boar': 1, 'Aqua': 2, 'MatchstickSeller': 3, 'KingOfBirds': 4, 'Elvis': 5,
-                   'MrSticky': 6, 'HighSchool': 7, 'Roswell': 8, 'Tulips': 9, 'Tourette': 10}
+class NaturalisticStories:
+    story_item_mapping = {'Boar': 1, 'Aqua': 2, 'MatchstickSeller': 3, 'KingOfBirds': 4, 'Elvis': 5,
+                          'MrSticky': 6, 'HighSchool': 7, 'Roswell': 8, 'Tulips': 9, 'Tourette': 10}
+    item_story_mapping = {item: story for story, item in story_item_mapping.items()}
     sentence_end = ['.', '?', '!', ".'", "?'", "!'"]
 
-    def __init__(self, stimulus_set_name,
-                 stimuli_filepath=os.path.join(_data_dir, 'naturalistic_stories', 'all_stories.tok')):
-        self._stimulus_set = self.set_mapping[stimulus_set_name]
+    def __init__(self, stimuli_filepath=_data_dir / 'naturalistic_stories' / 'all_stories.tok'):
         self._filepath = stimuli_filepath
 
-    def __call__(self, keep_meta=False):
+    def __call__(self, keep_meta=True):
         data = pd.read_csv(self._filepath, delimiter='\t')
 
         def words_to_sentences(words):
@@ -42,20 +51,20 @@ class NaturalisticStories(object):
             return pd.DataFrame({'sentence': sentences, 'sentence_num': list(range(len(sentences)))})
 
         data = data.groupby('item')['word'].apply(words_to_sentences).reset_index(level=0)
-        data = data[['item', 'sentence_num', 'sentence']]
-        data = data[data['item'] == self._stimulus_set]
-        if keep_meta:
-            return data
-        return data['sentence'].values
+        data['story'] = [self.item_story_mapping[item] for item in data['item']]
+        data = data[['story', 'item', 'sentence_num', 'sentence']]
+        data = StimulusSet(data)
+        data.name = 'naturalistic_stories'
+        return data
 
 
-class NaturalisticStoriesNeural(object):
+class NaturalisticStoriesNeural:
     def __init__(self, stimulus_set_name, reduced=False):
         self._stimulus_set_name = stimulus_set_name
         self._reduced = reduced
 
     def __call__(self):
-        from neural_nlp.neural_data import load_sentences_meta
+        from neural_nlp.neural_data.fmri import load_sentences_meta
         sentences_meta = load_sentences_meta(self._stimulus_set_name)
         return (sentences_meta['reducedSentence'] if self._reduced else sentences_meta['fullSentence']).dropna().values
 
@@ -68,8 +77,9 @@ _mappings = {
     **{'diverse.{}'.format(name): DiverseSentences(filename)
        for name, filename in DiverseSentences.set_mapping.items()},
     **{'naturalistic.{}'.format(name): NaturalisticStories(name)
-       for name in NaturalisticStories.set_mapping},
+       for name in NaturalisticStories.story_item_mapping},
     **{'naturalistic-neural-full.{}'.format(name): NaturalisticStoriesNeural(name, reduced=False)
-       for name in NaturalisticStories.set_mapping},
+       for name in NaturalisticStories.story_item_mapping},
     **{'naturalistic-neural-reduced.{}'.format(name): NaturalisticStoriesNeural(name, reduced=True)
-       for name in NaturalisticStories.set_mapping}}
+       for name in NaturalisticStories.story_item_mapping},
+    **{'concept-similarity': ConceptSimilarity()}}
