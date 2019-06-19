da97a15e31718248a8971cd1cc66186e8eb440e7
Martin Schrimpf <m4rtinsch@gmail.com>
2019-06-19 17:47:37 -0400

    no more need to store activations, new activations extractor takes care of that

--- neural_nlp/models/__init__.py
+++ neural_nlp/models/__init__.py
@@ -1,33 +1,29 @@
-import argparse
 import logging
 import sys
 
-from result_caching import store_xarray
+import argparse
+from brainio_base.assemblies import merge_data_arrays
 
-from brainscore.assemblies import merge_data_arrays
 from neural_nlp.models.implementations import _model_mappings, load_model, model_layers
-from neural_nlp.stimuli import _mappings, load_stimuli
 
 _logger = logging.getLogger(__name__)
 
 
-@store_xarray(identifier_ignore=['layers'], combine_fields={'layers': 'layer', 'sentences': 'sentence'})
-def get_activations(model_name, layers, stimulus_set_name):
-    _logger.debug('Loading model')
-    model = load_model(model_name)
-    _logger.debug('Loading stimuli')
-    stimuli = load_stimuli(stimulus_set_name)
+def get_activations(model_identifier, layers, stimuli):
+    _logger.debug(f'Loading model {model_identifier}')
+    model = load_model(model_identifier)
 
     _logger.debug("Retrieving activations")
-    activations = []
-    for i, sentence in enumerate(stimuli, start=1):
-        if (i - 1) % 10 == 0:
-            _logger.debug("Sentence {}/{} ({:.0f}%)".format(i, len(stimuli), 100 * i / len(stimuli)))
-        sentence_activations = model.get_activations([sentence], layers=layers)
-        assert len(sentence_activations) == 1
-        activations.append(sentence_activations)
+    activations = model(stimuli, layers=layers)
+    return activations
 
-    _logger.debug("Merging assemblies")
+
+def get_activations_for_sentence(model_name, layers, sentences):
+    model = load_model(model_name)
+    activations = []
+    for sentence in sentences:
+        sentence_activations = model.get_activations([sentence], layers=layers)
+        activations.append(sentence_activations)
     activations = merge_data_arrays(activations)
     return activations
 
@@ -36,7 +32,7 @@ def main():
     parser = argparse.ArgumentParser('model activations')
     parser.add_argument('--model', type=str, required=True, choices=list(_model_mappings.keys()))
     parser.add_argument('--layers', type=str, nargs='+', default='default')
-    parser.add_argument('--dataset', type=str, required=True, choices=list(_mappings.keys()))
+    parser.add_argument('--sentence', type=str, required=True)
     parser.add_argument('--log_level', type=str, default='INFO')
     args = parser.parse_args()
     log_level = logging.getLevelName(args.log_level)
@@ -45,7 +41,7 @@ def main():
         args.layers = model_layers[args.model]
     _logger.info("Running with args %s", vars(args))
 
-    activations = get_activations(args.model, layers=args.layers, stimulus_set_name=args.dataset)
+    activations = get_activations_for_sentence(args.model, layers=args.layers, sentences=args.sentence.split(' '))
     _logger.info("Activations computed: {}".format(activations))
 
 
