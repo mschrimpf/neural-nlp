3050b33a0344cf74ee8d0baa1fd296d949486d70
Martin Schrimpf <m4rtinsch@gmail.com>
2019-06-20 14:17:35 -0400

    plot number of used voxels per subject

---
+++ neural_nlp/analyze/subject_voxels.py
@@ -0,0 +1,28 @@
+import os
+
+import seaborn
+from matplotlib import pyplot
+
+from neural_nlp import NaturalisticStoriesBenchmark
+
+seaborn.set()
+
+
+def main():
+    benchmark = NaturalisticStoriesBenchmark()
+    data = benchmark._target_assembly
+    subject_voxels = {}
+    for subject in set(data['subject_UID'].values):
+        subject_assembly = data.sel(subject_UID=subject)
+        subject_voxels[subject] = len(subject_assembly['neuroid'])
+    fig, ax = pyplot.subplots()
+    ax.bar(subject_voxels.keys(), subject_voxels.values())
+    ax.set_xlabel('subject')
+    ax.set_ylabel('number of used neurons')
+    savepath = os.path.join(os.path.dirname(__file__), 'subject_voxels.png')
+    pyplot.savefig(savepath)
+    print(f"Saved to {savepath}")
+
+
+if __name__ == '__main__':
+    main()
