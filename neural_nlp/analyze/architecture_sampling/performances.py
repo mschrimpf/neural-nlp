a8753b948c47ff084f126fcc092005333c1d3ec3
Martin Schrimpf <m4rtinsch@gmail.com>
2019-08-19 12:05:24 -0400

    separate data collection

--- neural_nlp/analyze/architecture_sampling/performances.py
+++ neural_nlp/analyze/architecture_sampling/performances.py
@@ -2,43 +2,32 @@ import json
 import logging
 import warnings
 
+import fire
 import numpy as np
+import pandas as pd
+import seaborn
 from matplotlib import pyplot
 from pathlib import Path
 from tqdm import tqdm
 
-import seaborn
+from result_caching import store
+
 seaborn.set()
 
 _logger = logging.getLogger(__name__)
 
 
-def main(data_dir):
-    data_dir = Path(data_dir)
-    model_dirs = list(data_dir.iterdir())
-    validation_perplexities = []
-    no_result = 0
-    for model_dir in tqdm(model_dirs, desc='models'):
-        log_file = (model_dir / 'log')
-        if not log_file.exists():
-            warnings.warn(f"Log file {log_file} does not exist")
-            continue
-        logs = log_file.read_text().split('\n')
-        logs = [json.loads(log) for log in logs if log]
-        val_ppls = [log for log in logs if log['data'] == 'valid']
-        if not val_ppls:
-            no_result += 1
-            continue
-        for val_ppl in val_ppls:
-            epoch = val_ppl['epoch']
-            if not (model_dir / f"checkpoint-epoch{epoch}.pt").exists():
-                continue
-            validation_perplexities.append(val_ppl['ppl'])
+def plot_histograms(data_dirs=('/braintree/data2/active/users/msch/zoo.bck20190408-multi30k',
+                               '/braintree/data2/active/users/msch/zoo.wmt17')):
+    for data_dir in data_dirs:
+        plot_histogram(data_dir)
 
-    if no_result:
-        warnings.warn(f"Found {no_result} logs without validation perplexity")
 
-    validation_perplexities = np.array(validation_perplexities)
+def plot_histogram(data_dir):
+    data = collect(data_dir)
+    data = data[data['data'] == 'valid']
+    data = data.loc[data.groupby('model_dir')['epoch'].idxmax()]
+    validation_perplexities = data['ppl'].values
     validation_perplexities = validation_perplexities[~np.isnan(validation_perplexities)]
 
     fig, axes = pyplot.subplots(nrows=2, ncols=1)
@@ -51,9 +40,37 @@ def main(data_dir):
     axes[1].set_xlabel('validation perplexity')
     axes[1].set_ylabel('# architectures')
 
-    pyplot.savefig(Path(__file__).parent / f"validation_perplexities-{data_dir.name}.png")
+    pyplot.savefig(Path(__file__).parent / f"validation_perplexities-{Path(data_dir).name}.png")
+
+
+@store()
+def collect(data_dir):
+    data_dir = Path(data_dir)
+    model_dirs = list(data_dir.iterdir())
+    data_rows = []
+    for model_dir in tqdm(model_dirs, desc='models'):
+        log_file = (model_dir / 'log')
+        if not log_file.exists():
+            warnings.warn(f"Log file {log_file} does not exist")
+            continue
+        logs = log_file.read_text().split('\n')
+        logs = [json.loads(log) for log in logs if log]
+        for log in logs[1:]:  # ignore first log (model hyper-parameters)
+            if 'epoch' not in log:
+                warnings.warn(f"Ignoring log due to missing epoch value: {log}")
+                continue
+            if not (model_dir / f"checkpoint-epoch{log['epoch']}.pt").exists():
+                warnings.warn(f"Ignoring {model_dir}/epoch {log['epoch']} because checkpoint file does not exist")
+                continue
+            data_rows.append({
+                'model_dir': model_dir,
+                'epoch': log['epoch'],
+                'data': log['data'],
+                'ppl': log['ppl'],
+            })
+    data_rows = pd.DataFrame(data_rows)
+    return data_rows
 
 
 if __name__ == '__main__':
-    main('/braintree/data2/active/users/msch/zoo.bck20190408-multi30k')
-    main('/braintree/data2/active/users/msch/zoo.wmt17')
+    fire.Fire()
