592651a3a8bef86ba7fd13d436565fc6443bfe65
Martin Schrimpf <m4rtinsch@gmail.com>
2019-08-30 10:55:32 -0400

    use model directly for activations; rename _run -> score

--- neural_nlp/__init__.py
+++ neural_nlp/__init__.py
@@ -6,29 +6,29 @@ from brainscore.metrics import Score
 from brainscore.metrics.transformations import apply_aggregate
 from neural_nlp import models
 from neural_nlp.benchmarks import benchmark_pool
-from neural_nlp.models import get_activations, model_layers
+from neural_nlp.models import get_activations, model_layers, model_pool, SubsamplingHook
 from neural_nlp.neural_data.fmri import load_rdm_sentences as load_neural_rdms, load_voxels
 from result_caching import store
 
 _logger = logging.getLogger(__name__)
 
 
-@store(identifier_ignore=['layers', 'prerun'])
-def _run(benchmark, model, layers, subsample=None, prerun=True, bold_shift=4):
-    _logger.info('Running benchmark')
+@store(identifier_ignore=['layers', 'prerun', 'model_impl'])
+def score(benchmark, model, layers=None, model_impl=None, subsample=None, bold_shift=4):
+    model_impl = model_impl or model_pool[model]
+    if subsample:
+        SubsamplingHook.hook(model, subsample)
+    layers = layers or model_layers[model]
+
+    _logger.info('Loading benchmark')
     benchmark = benchmark_pool[benchmark](bold_shift=bold_shift)
     if hasattr(benchmark, 'ceiling'):  # not yet implemented for all
         print(benchmark.ceiling)
 
-    if prerun:
-        stimulus_set = benchmark._target_assembly.stimulus_set
-        get_activations(model_identifier=model, layers=layers, subsample=subsample,
-                        stimuli=stimulus_set, stimuli_identifier=stimulus_set.name)
-
+    _logger.info('Running')
     layer_scores = []
     for layer in tqdm(layers, desc='layers'):
-        candidate = lambda stimuli: get_activations(
-            model_identifier=model, layers=[layer], subsample=subsample, stimuli=stimuli)
+        candidate = lambda stimuli: model_impl(layers=[layer], stimuli=stimuli)
         layer_score = benchmark(candidate)
         layer_score = layer_score.expand_dims('layer')
         layer_score['layer'] = [layer]
