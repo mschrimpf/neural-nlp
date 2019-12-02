5d78d22b39f295bbde2588edc4416f29dafb555e
Martin Schrimpf <m4rtinsch@gmail.com>
2019-12-02 09:26:05 -0500

    add subject_stimuli plot: for each subject, show which stimuli have no nan voxels

--- neural_nlp/analyze/subject_voxels.py
+++ neural_nlp/analyze/subject_voxels.py
@@ -1,15 +1,35 @@
+import numpy as np
 import os
 
 import seaborn
 from matplotlib import pyplot
 
-from neural_nlp import VoxelBenchmark
+from neural_nlp.benchmarks.neural import StoriesVoxelEncoding, PereiraEncoding
+import fire
 
 seaborn.set()
 
 
-def main():
-    benchmark = VoxelBenchmark()
+def subject_stimuli():
+    benchmark = PereiraEncoding()
+    data = benchmark._target_assembly
+
+    def apply(group, axis=1):
+        return ~np.isnan(group).any(axis=axis)
+
+    data = data.groupby('subject').reduce(apply, dim='neuroid')
+    data = data.transpose('presentation', 'subject')
+    fig, ax = pyplot.subplots(figsize=(3, 21))
+    ax.imshow(data)
+    ax.set_xlabel('subject')
+    ax.set_ylabel('presentation')
+    savepath = os.path.join(os.path.dirname(__file__), 'subject_stimuli.png')
+    pyplot.savefig(savepath)
+    print(f"Saved to {savepath}")
+
+
+def subject_voxels():
+    benchmark = StoriesVoxelEncoding()
     data = benchmark._target_assembly
     subject_voxels = {}
     for subject in set(data['subject_UID'].values):
@@ -25,4 +45,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    fire.Fire()
