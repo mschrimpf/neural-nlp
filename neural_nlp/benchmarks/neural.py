3f0609542982931a2281674c21fc3ff078ee2c1b
grtu <grtu@dtu.dk>
2019-12-02 20:44:55 -0500

    Update neural.py

--- neural_nlp/benchmarks/neural.py
+++ neural_nlp/benchmarks/neural.py
@@ -17,6 +17,7 @@ from brainscore.metrics.transformations import CartesianProduct, CrossValidation
     apply_aggregate
 from brainscore.metrics.xarray_utils import XarrayRegression
 from neural_nlp.neural_data.fmri import load_voxels, load_rdm_sentences, load_Pereira2018
+from neural_nlp.neural_data.ecog_greta import load_Fedorenko2016
 from neural_nlp.stimuli import load_stimuli, StimulusSet
 from neural_nlp.utils import ordered_set
 from result_caching import store
@@ -381,6 +382,131 @@ class PereiraRDM(_PereiraBenchmark):
             crossvalidation_kwargs=dict(split_coord='stimulus_id', stratification_coord=None, splits=3))
         super(PereiraRDM, self).__init__(metric=metric)
 
+def read_words(candidate, stimulus_set): # This is a new version of the listen_to_stories function
+    # Input: stimulus_set = pandas df, col 1 with sentence ID and 2nd col as word.
+    activations = []
+    for word in ordered_set(stimulus_set['word'].values):
+        word_stimuli = stimulus_set[stimulus_set['word'] == word]
+#        story_stimuli.name = f"{stimulus_set.name}-{story}"
+        word_activations = candidate(stimuli=word_stimuli)
+        activations.append(word_activations)
+    model_activations = merge_data_arrays(activations)
+    # merging does not maintain stimulus order. the following orders again
+    idx = [model_activations['stimulus_id'].values.tolist().index(stimulus_id) for stimulus_id in
+           itertools.chain.from_iterable(s['stimulus_id'].values for s in activations)]
+    assert len(set(idx)) == len(idx), "Found duplicate indices to order activations"
+    model_activations = model_activations[{'presentation': idx}]
+    
+    return model_activations
+        
+        
+class FedorenkoBenchmark:
+    def __init__(self, bold_shift=None):
+        assembly = load_Fedorenko2016() 
+        self._target_assembly = assembly
+        
+        self._regression = pls_regression(xarray_kwargs=dict(stimulus_coord='timepoint1')) # word
+        self._correlation = pearsonr_correlation(xarray_kwargs=dict(correlation_coord='timepoint1'))
+        self._metric = CrossRegressedCorrelation(
+            regression=self._regression, correlation=self._correlation,
+            crossvalidation_kwargs=dict(split_coord='timepoint1', stratification_coord='timepoint1'))
+
+    @property
+    @store()
+    def ceiling(self):
+        cross_validation = CrossValidation(split_coord='stimulus_id', stratification_coord='timepoint1', splits=2) # still assuming a stratification cord along the word/timepoint dimension
+
+        def ceiling_apply(train_source, train_target, test_source, test_target):
+            self._regression.fit(train_source, train_target)
+            prediction = self._regression.predict(test_source)
+            score = self._correlation(prediction, test_target)
+            return score
+
+        subjects = list(sorted(set(self._target_assembly['subject_UID'].values))) # the subjectUID from the ECoG assembly packaging
+        split_scores = []
+        
+        for heldout_subject in tqdm(subjects, desc='subject holdout'):
+            subject_pool = list(sorted(set(subjects) - {heldout_subject}))
+            indexer_pool = DataArray(np.zeros(len(subject_pool)), coords={'subject_UID': subject_pool},
+                                     dims=['subject_UID']).stack(neuroid=['subject_UID'])
+            heldout_indexer = DataArray(np.zeros(1), coords={'subject_UID': [heldout_subject]},
+                                        dims=['subject_UID']).stack(neuroid=['subject_UID'])
+            subject_pool = subset(self._target_assembly, indexer_pool, dims_must_match=False)
+            heldout = subset(self._target_assembly, heldout_indexer, dims_must_match=False)
+            split_score = cross_validation(subject_pool, heldout, apply=ceiling_apply, aggregate=self._metric.aggregate)
+            split_score = split_score.expand_dims('heldout_subject')
+            split_score['heldout_subject'] = [heldout_subject]
+            split_score.attrs[Score.RAW_VALUES_KEY] = split_score.attrs[Score.RAW_VALUES_KEY]
+            split_scores.append(split_score)
+        consistency = Score.merge(*split_scores)
+        error = standard_error_of_the_mean(consistency.sel(aggregation='center'), 'heldout_subject')
+        consistency = apply_aggregate(lambda scores: scores.mean('heldout_subject'), consistency)
+        consistency.loc[{'aggregation': 'error'}] = error
+        return consistency        
+        
+        
+    def __call__(self, candidate):
+        _logger.info('Computing activations')
+        
+        stimulus_set = self._target_assembly.attrs['stimulus_set']
+        
+        model_activations = read_words(candidate, stimulus_set)
+        assert (model_activations['timepoint1'].values == self._target_assembly['timepoint1'].values).all()
+        return self._metric(model_activations, self._target_assembly)
+
+    
+    
+class FedorenkoBenchmarkMean:
+    def __init__(self, bold_shift=None):
+        assembly = load_Fedorenko2016() 
+        self._target_assembly = assembly
+                
+        # avg code 
+        # packaging file, change the benchmark to include averaging. 
+        
+        self._regression = pls_regression(xarray_kwargs=dict(stimulus_coord='timepoint1')) # word
+        self._correlation = pearsonr_correlation(xarray_kwargs=dict(correlation_coord='timepoint1'))
+        self._metric = CrossRegressedCorrelation(
+            regression=self._regression, correlation=self._correlation,
+            crossvalidation_kwargs=dict(split_coord='timepoint1', stratification_coord='timepoint1'))
+    
+    def __call__(self, candidate):
+        
+        _logger.info('Computing activations')
+        
+        stimulus_set = self._target_assembly.attrs['stimulus_set']
+        
+        model_activations = read_words(candidate, stimulus_set)
+        assert (model_activations['timepoint1'].values == self._target_assembly['timepoint1'].values).all()
+        
+        # average here, can I simply hard code the averaging across sentences?
+        # model_activations = 
+        
+        print('Model activations:   ', model_activations)
+        
+        return self._metric(model_activations, self._target_assembly)
+        
+        
+#         scores = []
+#         for story, story_assembly in self._target_assemblies.items():
+#             stimulus_set = load_stimuli(f'naturalistic-neural-reduced.{story}')
+#             source_assembly = candidate(stimuli=stimulus_set)
+#             story_assembly_rdm = self._metric._rdm(story_assembly)
+#             score = self._metric(source_assembly, story_assembly_rdm)
+#             score = score.expand_dims('story')
+#             score['story'] = [story]
+#             scores.append(score)
+#         score = Score.merge(*scores)
+#         score = apply_aggregate(lambda score: score.mean('story'), score)
+#         return score
+
+    
+    def ceiling(self):
+        return None
+    
+    
+    
+    
 
 benchmark_pool = {
     'voxel-encoding': StoriesVoxelEncoding,
@@ -394,4 +520,6 @@ benchmark_pool = {
     'Pereira2018-decoding': PereiraDecoding,
     'Pereira2018-rdm': PereiraRDM,
     'transformer-wordmean': StoriesTransformerWordmeanBenchmark,
+    'Fedorenko2016': FedorenkoBenchmark,
+    'Fedorenko2016Mean': FedorenkoBenchmarkMean,
 }
