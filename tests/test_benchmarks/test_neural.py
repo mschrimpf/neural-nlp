29b0196de1e9d40e191598ad7f500e1476f999dd
Martin Schrimpf <m4rtinsch@gmail.com>
2020-02-03 09:06:42 -0500

    update ceiling scores with full KFold + linear regression

--- tests/test_benchmarks/test_neural.py
+++ tests/test_benchmarks/test_neural.py
@@ -1,16 +1,18 @@
 import pytest
 from pytest import approx
 
-from neural_nlp.benchmarks.neural import PereiraEncoding, PereiraRDM, Fedorenko2016Encoding, \
+from neural_nlp.benchmarks.neural import PereiraEncoding, PereiraRDM, \
+    Fedorenko2016Encoding, Fedorenko2016AllEncoding, \
     StoriesVoxelEncoding, StoriesfROIEncoding
 
 
 @pytest.mark.parametrize('benchmark_ctr, expected', [
-    (PereiraEncoding, 0.331542),
+    (PereiraEncoding, 0.292317),
     (PereiraRDM, 0.048785),
-    (Fedorenko2016Encoding, .125773),
-    (StoriesVoxelEncoding, .129175),
-    (StoriesfROIEncoding, .145968),
+    (Fedorenko2016Encoding, .098452),
+    (Fedorenko2016AllEncoding, .178473),
+    (StoriesVoxelEncoding, .086608),
+    (StoriesfROIEncoding, .156544),
 ])
 def test_ceiling(benchmark_ctr, expected):
     benchmark = benchmark_ctr()
