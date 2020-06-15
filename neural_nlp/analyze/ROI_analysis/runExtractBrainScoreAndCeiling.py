3f72ca570a1aa18e0f735e7f0f355e7d36232c7c
Greta Tuckute <greta-t@hotmail.com>
2020-06-15 12:03:59 -0400

    remove old ROIwise analyses

--- neural_nlp/analyze/ROI_analysis/runExtractBrainScoreAndCeiling.py
+++
@@ -1,7 +0,0 @@
-from extractBrainScoreAndCeiling import extractBrainScoreAndCeiling
-
-extractBrainScoreAndCeiling(score_name = 'benchmark=Pereira2018-encoding,model=glove,subsample=None.pkl',
-                                ceil_name = False)
-    
-    
-print('Generated ROI-wise brain-score analysis for all subjects')
