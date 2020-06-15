5d1791ea8aa4672b55908b1ebefd72990a6a3072
Greta Tuckute <greta-t@hotmail.com>
2020-06-15 12:06:10 -0400

    reorganize dir structure in surface_projection

--- neural_nlp/analyze/surface_projection/Python/runCreateBrainMatrix.py
+++
@@ -1,5 +0,0 @@
-from createBrainMatrixFunc import createBrainMatrix
-
-for sub in ['018', '199', '215', '288', '289', '296', '343', '366', '407','426']:
-    createBrainMatrix(subjectID = sub, score_name = 'benchmark=Pereira2018-encoding,model=gpt2-xl,subsample=None.pkl')
-    print('Generated brain matrix for subject: ', sub)
