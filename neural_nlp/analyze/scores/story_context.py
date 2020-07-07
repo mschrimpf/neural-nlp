5f9d5cedcd86fc241ed8f131dacc21c0fddfb07e
Martin Schrimpf <m4rtinsch@gmail.com>
2020-07-07 11:16:36 -0400

    fix sentence numbers

--- neural_nlp/analyze/scores/story_context.py
+++ neural_nlp/analyze/scores/story_context.py
@@ -1,4 +1,5 @@
 import logging
+import numpy as np
 import pandas as pd
 import sys
 from functools import reduce
@@ -11,24 +12,25 @@ from neural_nlp.analyze.scores import choose_best_scores, collect_scores
 
 
 def plot_num_sentences(model='gpt2-xl'):
-    fig, axes = pyplot.subplots(ncols=2)
-    for ax, normalize in zip(axes, [True, False]):
+    fig, ax = pyplot.subplots(figsize=(4.5, 5.5))
+    for ax, normalize in zip([ax], [True]):
         scores = []
-        for num_sentences in [1, 3, 6, 9]:
-            benchmark = f'Blank2014sentences{num_sentences}fROI-encoding' if num_sentences != 9 \
+        sentence_nums = np.arange(1, 10, 2)
+        for sentence_num in sentence_nums:
+            benchmark = f'Blank2014sentence{sentence_num}fROI-encoding' if sentence_num != 9 \
                 else 'Blank2014fROI-encoding'  # standard benchmark is 9 sentences
             _scores = collect_scores(benchmark=benchmark, models=[model], normalize=normalize)
             _scores = choose_best_scores(_scores)
-            _scores['num_sentences'] = num_sentences
+            _scores['sentence_num'] = sentence_num
             ceiling = benchmark_pool[benchmark].ceiling
             _scores['ceiling'] = ceiling.sel(aggregation='center')
             scores.append(_scores)
         scores = reduce(lambda left, right: pd.concat([left, right]), scores)
-        ax.errorbar(x=scores['num_sentences'], y=scores['score'], yerr=scores['error'], label='scores')
+        ax.errorbar(x=scores['sentence_num'], y=scores['score'], yerr=scores['error'], label='scores')
+        ax.set_xticks(sentence_nums)
         ax.set_xlim(list(reversed(ax.get_xlim())))  # flip x
-        ax.set_xlabel("# sentences")
-        ax.set_ylabel(("Normalized " if normalize else "") + f"Consistency ({model})")
-        ax.set_title(("Ceiled" if normalize else "Raw") + " Scores")
+        ax.set_xlabel("sentence # in story")
+        ax.set_ylabel(("Normalized " if normalize else "") + f"Predictivity ({model})")
 
     savefig(fig, savename=Path(__file__).parent / "story_context")
 
