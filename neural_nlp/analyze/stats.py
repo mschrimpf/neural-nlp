9870170b29357635bca48fcd6e15de89b293de7d
Martin Schrimpf <m4rtinsch@gmail.com>
2020-07-07 11:21:07 -0400

    remove dead code

--- neural_nlp/analyze/stats.py
+++ neural_nlp/analyze/stats.py
@@ -10,15 +10,13 @@ logger = logging.getLogger(__name__)
 def is_significant(scores, reference_scores, samples=10000, pvalue_threshold=0.05):
     delta = np.mean(scores) - np.mean(reference_scores)
     print('Delta: ' + str(delta))
-    #if delta < 0:
-    #    return False  # reference scores are already higher, don't bother checking
     pooled = np.hstack([scores, reference_scores])
     estimates = np.array([_permutation_test(pooled, len(scores), len(reference_scores))
                           for _ in range(samples)])
     print('Shuffled estimates: ', str(np.mean(estimates)))
     diff = len(np.where(estimates <= delta)[0])
     p = 1.0 - diff / samples
-    return delta, np.mean(estimates), p 
+    return delta, np.mean(estimates), p
 
 
 def _permutation_test(pooled, size1, size2):
