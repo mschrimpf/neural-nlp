116f335c8a94463bc4706af7d72169a535a72fbb
Martin Schrimpf <m4rtinsch@gmail.com>
2020-07-07 11:44:23 -0400

    add multi-layer test

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -18,7 +18,7 @@ class TestScore:
         assert 'region' in scores.aggregation
         assert scores.aggregation.sel(aggregation='center').shape[0] == 44
 
-    @pytest.mark.parametrize("num_layers", [2])#1, 2])
+    @pytest.mark.parametrize("num_layers", [1, 2])
     def test_dummy_model(self, num_layers):
         model_layers = [f'dummylayer{i}' for i in range(num_layers)]
 
@@ -36,5 +36,22 @@ class TestScore:
 
         s = score(model=f'dummy-{num_layers}layers', model_impl=DummyModel(), layers=model_layers,
                   benchmark='Pereira2018-encoding-min')
-        print(s)
+        assert s.sel(aggregation='center') == approx(0, abs=.5)
+
+    def test_dummy_tuple_layers(self):
+        model_layers = [f'dummylayer{i}' for i in range(2)]
+
+        class DummyModel:
+            def __call__(self, stimuli, layers):
+                assembly = NeuroidAssembly(np.ones([len(stimuli), 1]), coords={
+                    'stimulus_sentence': ('presentation', stimuli['sentence']),
+                    'dummy': ('presentation', ['dummy'] * len(stimuli)),
+                    'neuroid_id': ('neuroid', [model_layers.index(layers[0])]),
+                    'layer': ('neuroid', layers),
+                }, dims=['presentation', 'neuroid'])
+                assembly = attach_stimulus_set_meta(assembly, stimuli)
+                return assembly
+
+        s = score(model=f'dummy-tuple_layers', model_impl=DummyModel(), layers=tuple(model_layers),
+                  benchmark='Pereira2018-encoding-min')
         assert s.sel(aggregation='center') == approx(0, abs=.5)
