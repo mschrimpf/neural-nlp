6a8e4333cc32eaa6b84cb79eb9ca340fa0404b96
Carina Kauf <57629697+carina-kauf@users.noreply.github.com>
2021-02-12 11:27:35 -0400

    GLUE on LM1B (#41)
    * GLUE on LM1B -- fixed getting last token representation
    
    * run glue sentences through lm1b directly
    
    * use lm_1b progress bar
    
    https://github.com/mschrimpf/lm_1b/commit/793f3d6e0d3f0554cf80f8486619d049a3d77eb6
    
    Co-authored-by: Martin Schrimpf <m4rtinsch@gmail.com>

--- neural_nlp/models/implementations.py
+++ neural_nlp/models/implementations.py
@@ -441,12 +441,12 @@ class LM1B(BrainModel, TaskModel):
                                                          self._encoder.t['target_weights_in']: weights})
             if i > 0:  # 0 is <S>
                 embeddings.append(lstm_emb)
-        # `embeddings` shape is now: words x layers x *layer_shapes
+            #`embeddings` shape is now: words x layers x *layer_shapes
         layer_activations = {}
         for i, layer in enumerate(layers):
             # embeddings is `words x layers x (1 x 1024)`
             layer_activations[layer] = [embedding[i] for embedding in embeddings]
-            # words x 1 x 1024 --> words x 1024
+            # `words x 1 x 1024` --> `words x 1024`
             layer_activations[layer] = np.array(layer_activations[layer]).transpose(1, 0, 2).squeeze(axis=0)
         return layer_activations
 
@@ -455,6 +455,43 @@ class LM1B(BrainModel, TaskModel):
 
     def available_layers(self, filter_inputs=True):
         return [tensor_name for tensor_name in self._encoder.t if not filter_inputs or not tensor_name.endswith('_in')]
+
+    def glue_dataset(self, examples, label_list, output_mode):
+        import torch
+        from torch.utils.data import TensorDataset
+        label_map = {label: i for i, label in enumerate(label_list)}
+        features = []
+
+        if examples[0].text_b is not None:
+            text_a = [example.text_a for example in examples]
+            text_b = [example.text_b for example in examples]
+            sents1 = self._encoder(text_a, progress_bar=True)[0]
+            sents2 = self._encoder(text_b, progress_bar=True)[0]
+            for sent1, sent2 in zip(sents1, sents2):
+                sent1 = torch.tensor(sent1[-1].squeeze())
+                sent2 = torch.tensor(sent2[-1].squeeze())
+                f = torch.cat([sent1, sent2, torch.abs(sent1 - sent2), sent1 * sent2], -1)
+                features.append(PytorchWrapper._tensor_to_numpy(f))
+            all_features = torch.tensor(features)
+
+        else:
+            text_a = [example.text_a for example in examples]
+            # encoder returns a tuple (sentences_embeddings, sentences_word_ids).
+            # the sentences_embeddings encode every single word with a 1x1024 embedding,
+            # so we use the last word's embedding and squeeze the singular dimension.
+            sents = self._encoder(text_a, progress_bar=True)[0]
+            for sent in sents:
+                sent = torch.tensor(sent[-1].squeeze())
+                features.append(PytorchWrapper._tensor_to_numpy(sent))
+            all_features = torch.tensor(features)
+
+        if output_mode == "classification":
+            all_labels = torch.tensor([label_map[example.label] for example in examples], dtype=torch.long)
+        elif output_mode == "regression":
+            all_labels = torch.tensor([label_map[example.label] for example in examples], dtype=torch.float)
+
+        dataset = TensorDataset(all_features, all_labels)
+        return dataset
 
     default_layers = ['lstm/lstm_0/control_dependency', 'lstm/lstm_1/control_dependency']
 
